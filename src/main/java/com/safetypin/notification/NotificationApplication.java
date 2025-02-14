4fd9a6ecab380b13e2b415e96141238cef8967d7
Muhammad Raihan Akbar <ianakbar711@gmail.com>
2025-02-14 19:56:52 +0700

    add: Hello World

--- src/main/java/com/safetypin/notification/NotificationApplication.java
+++ src/main/java/com/safetypin/notification/NotificationApplication.java
@@ -2,6 +2,9 @@ package com.safetypin.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class NotificationApplication {
@@ -10,4 +13,13 @@ public class NotificationApplication {
 		SpringApplication.run(NotificationApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequestMapping("/")
+class HelloController {
+	@GetMapping
+	public String sayHello() {
+		return "Hello, World!";
+	}
 }
